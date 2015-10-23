ef2fad85ffacb9a204a7e3376bd9c98cecd27fca
Elin <elinnet@gmail.com>
2015-10-23 18:20:18 +0100

    add cfprototype files"

---
+++ expressapp/app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname +'/public'));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/new', function (req, res){
+  fs.readFile('./strength.json', 'utf8', function(err,data){
+    if (err) throw err;
+    var training = JSON.parse(data);
+    res.send(data);
+
+  });
+
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
---
+++ testapp/www/js/controllers.js
@@ -0,0 +1,80 @@
+angular.module('starter.controllers', [])
+
+.controller('WodCtrl', function($scope, $http) {
+  // console.log($http);
+  //
+  // $scope.training = function(){
+  //   console.log("working");
+  //   $scope.workout=[];
+  //   $http.get('http://errand-runner.herokuapp.com/tasks').
+  //   success(function(data) {
+  //       $scope.workout.push(data);
+  //     }).
+  //       error(function(data){
+  //         console.log("error");
+  //       });
+  //   };
+
+    ionic.Platform.ready(function(){
+    console.log("inside ready function");
+
+  $scope.training = function(){
+    console.log("working");
+    $scope.workout=[];
+    $http.get('http://localhost:3000/new').
+    success(function(data) {
+        $scope.workout.push(data);
+      }).
+        error(function(data){
+          console.log("error");
+        });
+    };
+});
+  //
+
+
+
+    // {
+    //   "day": "Mon 06/10/15",
+    //   "box": "CF Hackney",
+    //   "strength": "12 min E2MOM, 6 rounds of:, 1 Power Clean, 2 Split Jerks",
+    //   "metcon": "4 rounds of:, 3 min AMRAP of:, 2 Power Clean and Jerk, 4 Over the Bar Burpees"
+    // },
+    // {
+    //   "day": "Tues 07/10/15",
+    //   "box": "CF Hackney",
+    //   "Strength1": "Strength",
+    //   "Metcon": "Metcon"
+    // },
+  //
+  //
+  // $scope.currentDayStrength = $scope.workout[0].strength.split(", ");
+  //
+  // $scope.currentDayMetcon = $scope.workout[0].metcon.split(", ");
+
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
