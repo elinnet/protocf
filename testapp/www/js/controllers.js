34dd49fe6f8fb4f265070939b05396c8b6003029
Elin <elinnet@gmail.com>
2015-10-24 15:31:27 +0100

    formatted strength section

--- testapp/www/js/controllers.js
+++ testapp/www/js/controllers.js
@@ -12,54 +12,22 @@ angular.module('starter.controllers', [])
         success(function(data) {
             $scope.workout.push(data);
             console.log(data);
+            console.log(data.date);
             console.log($scope.workout.push(data));
           }).
             error(function(data){
               console.log("error");
             });
       };
-
-
-      // var strengthHeader = workout[0].box.info.name;
-});
-  //
-
-
-
-    // {
-    //   "day": "Mon 06/10/15",
-    //   "box": "CF Hackney",
-    //   "strength": "12 min E2MOM, 6 rounds of:, 1 Power Clean, 2 Split Jerks",
-    //   "metcon": "4 rounds of:, 3 min AMRAP of:, 2 Power Clean and Jerk, 4 Over the Bar Burpees"
-    // },
-    // {
-    //   "day": "Tues 07/10/15",
-    //   "box": "CF Hackney",
-    //   "Strength1": "Strength",
-    //   "Metcon": "Metcon"
-    // },
-  //
-  //
-  // $scope.currentDayStrength = $scope.workout[0].strength.split(", ");
-  //
-  // $scope.currentDayMetcon = $scope.workout[0].metcon.split(", ");
-
+    });
 })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+// .controller('ChatsCtrl', function($scope, Chats) {
+//   $scope.chats = Chats.all();
+//   $scope.remove = function(chat) {
+//     Chats.remove(chat);
+//   };
+// })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
