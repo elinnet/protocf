5ed21ffd9d93d62fd87e8f51c4a2dac269641927
Elin <elinnet@gmail.com>
2015-10-26 19:27:16 +0000

    on days programme shows on app

--- testapp/www/js/controllers.js
+++ testapp/www/js/controllers.js
@@ -5,23 +5,6 @@ angular.module('starter.controllers', [])
 
     $scope.workout = Workout;
 
-    // ionic.Platform.ready(function(){
-    //   console.log("inside ready function");
-    //
-    //   $scope.training = function(){
-    //     console.log("working");
-    //     $scope.workout=[];
-    //     $http.get('http://localhost:3000/new').
-    //     success(function(data) {
-    //         $scope.workout.push(data);
-    //         console.log(data);
-    //       }).
-    //         error(function(data){
-    //           console.log("error");
-    //         });
-    //   };
-    // });
-
     $scope.format = function(string){
       console.log(string);
       // var newString = string.replace(/\./g,"\n");
--- testapp/www/js/factory.js
+++ testapp/www/js/factory.js
@@ -1,6 +1,6 @@
 app = angular.module('starter.factory',[]);
 
 app.factory('Workout', function($firebaseArray){
-  var itemsRef = new Firebase("https://incandescent-inferno-5080.firebaseio.com/items");
+  var itemsRef = new Firebase("https://incandescent-inferno-5080.firebaseio.com");
   return $firebaseArray(itemsRef);
 });
