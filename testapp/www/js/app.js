3a9f3d8c70f44f82b701080d770c400142f124a6
Elin <elinnet@gmail.com>
2015-10-26 08:36:18 +0000

    added firebase as an app dependancy and also in html

--- testapp/www/js/app.js
+++ testapp/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','starter.filters','starter.factory'])
+angular.module('starter', ['ionic', 'firebase', 'starter.controllers', 'starter.services','starter.filters','starter.factory'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- testapp/www/js/factory.js
+++ testapp/www/js/factory.js
@@ -1,4 +1,4 @@
-var app = angular.module('starter.factory',[]);
+app = angular.module('starter.factory',[]);
 
 app.factory('Workout', function($firebaseArray){
   var itemsRef = new Firebase("https://incandescent-inferno-5080.firebaseio.com/items");
