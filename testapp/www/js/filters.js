0e46d2340b03a94b8b3b6b064b6c6a673a914d13
Elin <elinnet@gmail.com>
2015-10-29 20:56:15 +0000

    save of files

--- testapp/www/js/filters.js
+++ testapp/www/js/filters.js
@@ -2,6 +2,9 @@ var app = angular.module('starter.filters',[]);
 
 app.filter('myFilter',function(){
   return function(input){
+    if (input === undefined){
+      return;
+    }
     return input.replace(/\./g, '\n');
   };
 });
